b534ba7946f24e8d3ca19716148c322d387f0ef6
luigibonati <lui.bonati@gmail.com>
2021-09-16 17:08:40 +0200

    added folders in header of cpp interface (for LOAD)

--- plumed-libtorch-interface/PytorchModel.cpp
+++ plumed-libtorch-interface/PytorchModel.cpp
@@ -19,8 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "Function.h"
-#include "ActionRegister.h"
+#include "function/Function.h"
+#include "core/ActionRegister.h"
 
 #include <torch/torch.h>
 #include <torch/script.h>
